8e79fc65a5be100aa746fadcc229c2804482388c
Xiaolu <kongbingji@163.com>
2023-04-11 11:25:47 -0600

    first commit

---
+++ src/main/java/com/comit/dailyExpenseRecord/DailyExpenseRecordApplication.java
@@ -0,0 +1,13 @@
+package com.comit.dailyExpenseRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyExpenseRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyExpenseRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comit/dailyExpenseRecord/DailyExpenseRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.dailyExpenseRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyExpenseRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
